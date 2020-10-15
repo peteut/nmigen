e66d0701da083124a19f6c2ca8fb3adcdfe9a1ad
peteut <alain.peteut@yahoo.com>
2020-10-15 07:39:39 +0000

    Deploying to gh-pages from  @ dfd85448d52a7d7fa40cbc7a176939f6e631c623 🚀

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1.dev1130',
+    VERSION: '0.0.1.dev1131',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
